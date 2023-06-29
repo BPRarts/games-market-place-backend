052f4f8b8dc3d2bcb96389fc4248d7d3ea8c9254
Bryan <bryanpablo472@gmail.com>
2023-06-29 12:25:33 -0700

    origins update

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     
-    'https://games-market-place.onrender.com'
+    'https://games-market-place.onrender.com',
+    'https://games-market-place-backend.onrender.com'
 ]
 
 module.exports = allowedOrigins
